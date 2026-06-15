fd51f83b5282bc9153a4af6e4348088116b18aea
TonnyMCH <90722910+TonnyMCH@users.noreply.github.com>
2026-06-15 09:35:51 -0600

    Crear sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "biblioteca-v1";
+
+const archivos = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(archivos))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(resp => resp || fetch(e.request))
+  );
+});
